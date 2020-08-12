90b5b5d3d70534889a2b401a58747e30d0210761
Sidney Fletcher <sfletcher28@bloomberg.net>
2020-08-11 21:23:52 -0400

    add model support to webpage

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
+import dill as pickle
+import pandas as pd
 app = Flask(__name__)
+
+model = pickle.load(open("models/xgb_model.pkl", 'rb'))
+
 
 @app.route('/')
 def hello_world():
@@ -8,12 +13,12 @@ def hello_world():
 @app.route('/submit', methods = ['POST'])
 def submit_form():
     data = dict(request.form)
-    prob = predict_fraud(data)
+    prob = predict_fraud(data)[0][1]
     fr = "not fraudulent" if prob < 0.5 else "fradulent"
     msg = f"We think this transaction is {fr} with probability {prob: 0.2f}."
 
     return render_template('index.html', msg=msg)
 
 def predict_fraud(data: dict):
-    print(data)
-    return 0.6
+    df = pd.DataFrame([data])
+    return model.predict_proba(df)
